460233f1d408fa10ef22ce01ae2f9a227245dbe1
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-12-13 12:30:02 +0200

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
+// tailwind.config.js
 module.exports = {
-  darkMode: ["class"], 
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -80,6 +79,5 @@ module.exports = {
       },
     },
   },
-  // eslint-disable-next-line no-undef
   plugins: [require("tailwindcss-animate")],
 };
