0d72c04e811cd52642d1e743beb066630130f91c
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-12-13 12:25:20 +0200

    update toast.tsx and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ["class"], 
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
