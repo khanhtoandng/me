cce4b6e74dc5a0cd76ea82ec4239bd08e95a15b6
Baraa Alshaer <Baraa.Programmer@gmail.com>
2025-04-09 18:51:08 +0200

    update RecommendationsSection.tsx

--- src/components/website/RecommendationsSection.tsx
+++ src/components/website/RecommendationsSection.tsx
@@ -11,8 +11,7 @@ const RecommendationsSection = () => {
     {
       name: "Mohammed Abu Harb 🌀",
       title: "Digital Product Designer at LOGIX ERP",
-      image:
-        "https://media.licdn.com/dms/image/v2/D4D03AQFo5M71pj69Qw/profile-displayphoto-shrink_100_100/B4DZN95ApYHMAU-/0/1732983918432?e=1741824000&v=beta&t=Q6vjWiOAIb_4HtbqSyPurqBH3Wz_zHWZVNB880strIA",
+      image: "",
       review: `I’m thrilled to recommend Baraa, an incredibly skilled software developer I’ve had the pleasure of working with. Baraa's technical skills are top-notch, and his passion for development is evident in every project he tackles. Not only does he deliver high-quality work, but he also brings a positive attitude and strong problem-solving skills to the table. If you’re looking for someone who’s both skilled and dedicated, Baraa is your go-to developer. Highly recommended!`,
     },
     {
@@ -37,8 +36,8 @@ sam-tax.com
     {
       name: "Fahad Hummadi",
       title: "Co-Founder & CEO @ Perfect Touch",
-      image:
-        "https://media.licdn.com/dms/image/v2/D4D03AQG9oH7QfpaEwA/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1703751939226?e=1741824000&v=beta&t=vT-6fPiITv7O2wNfMyJ6wl8l0yiDJxhwqipFsjoRgcI",
+      image: "",
+
       review: `To Whom It May Concern,
 
 I am pleased to recommend Baraa, who worked as a Front-End Developer at PTIT company. During his time with us, Baraa consistently demonstrated exceptional technical skills in front-end development, with a keen eye for detail and a deep understanding of modern web technologies. Baraa was highly dedicated, always eager to learn, and contributed significantly to the success of our projects. His ability to collaborate effectively with both the development team and other departments made him a valuable asset to our company. He consistently met deadlines and produced high-quality work that exceeded our expectations. I am confident that Baraa will bring the same level of professionalism, commitment, and expertise to any team he joins. I highly recommend him for any front-end development role.`,
@@ -46,8 +45,8 @@ I am pleased to recommend Baraa, who worked as a Front-End Developer at PTIT com
     {
       name: "Ali Khaled",
       title: "Front-end Engineer",
-      image:
-        "https://media.licdn.com/dms/image/v2/D4D03AQF_AUH9ZCnYxg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1719186347762?e=1741824000&v=beta&t=BLtOPVOrl6AHTeH49XekqUgE7Arufi-jXZrxgtAanLQ",
+      image: "",
+
       review: `It was an absolute privilege to work with Baraa. His exceptional skills as a developer, combined with his dedication to delivering high-quality work, made every project a success. Baraa has a unique ability to solve complex problems efficiently, and his collaborative spirit makes him an asset to any team. I have no doubt that he will achieve great things in his career, and I would gladly work with him again. He comes highly recommended!`,
     },
   ];
