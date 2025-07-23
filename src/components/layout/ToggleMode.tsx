e7518eebd79617898821ddd8c53b8b1ca32a1ce0
Baraa Alshaer <baraa.programmer@gmail.com>
2025-07-23 17:32:34 +0300

    fix(vercel): resolve Vercel build error for successful deployment

---
+++ src/components/layout/ToggleMode.tsx
@@ -0,0 +1,13 @@
+import { ThemeToggleButton } from "../ui/theme-toggle-button";
+
+export default function ToggleMode() {
+  return (
+    <div>
+ 
+    <ThemeToggleButton showLabel variant="circle-blur" start="top-right" />
+
+
+    </div>
+  )
+}
+
